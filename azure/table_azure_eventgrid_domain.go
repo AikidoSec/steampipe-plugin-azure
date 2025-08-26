53fb802641a9b6a9d545a1b70c8d03eebab3fb54
NorbertHambuer <norbert@aikido.dev>
2025-08-26 16:28:43 +0300

    feat: Upgrade EventGrid domains SDK

--- azure/table_azure_eventgrid_domain.go
+++ azure/table_azure_eventgrid_domain.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/eventgrid/mgmt/eventgrid"
 	"github.com/Azure/azure-sdk-for-go/profiles/preview/preview/monitor/mgmt/insights"
+	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/eventgrid/armeventgrid/v2"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
@@ -64,19 +65,19 @@ func tableAzureEventGridDomain(_ context.Context) *plugin.Table {
 				Name:        "provisioning_state",
 				Description: "Provisioning state of the event grid domain resource. Possible values include: 'Creating', 'Updating', 'Deleting', 'Succeeded', 'Canceled', 'Failed'.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("DomainProperties.ProvisioningState"),
+				Transform:   transform.FromField("Properties.ProvisioningState"),
 			},
 			{
 				Name:        "auto_create_topic_with_first_subscription",
 				Description: "This Boolean is used to specify the creation mechanism for 'all' the event grid domain topics associated with this event grid domain resource.",
 				Type:        proto.ColumnType_BOOL,
-				Transform:   transform.FromField("DomainProperties.AutoCreateTopicWithFirstSubscription"),
+				Transform:   transform.FromField("Properties.AutoCreateTopicWithFirstSubscription"),
 			},
 			{
 				Name:        "auto_delete_topic_with_last_subscription",
 				Description: "This Boolean is used to specify the deletion mechanism for 'all' the Event Grid Domain Topics associated with this Event Grid Domain resource.",
 				Type:        proto.ColumnType_BOOL,
-				Transform:   transform.FromField("DomainProperties.AutoDeleteTopicWithLastSubscription"),
+				Transform:   transform.FromField("Properties.AutoDeleteTopicWithLastSubscription"),
 			},
 			{
 				Name:        "created_at",
@@ -100,13 +101,13 @@ func tableAzureEventGridDomain(_ context.Context) *plugin.Table {
 				Name:        "disable_local_auth",
 				Description: "This boolean is used to enable or disable local auth. Default value is false. When the property is set to true, only AAD token will be used to authenticate if user is allowed to publish to the domain.",
 				Type:        proto.ColumnType_BOOL,
-				Transform:   transform.FromField("DomainProperties.DisableLocalAuth"),
+				Transform:   transform.FromField("Properties.DisableLocalAuth"),
 			},
 			{
 				Name:        "endpoint",
 				Description: "Endpoint for the Event Grid Domain Resource which is used for publishing the events.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("DomainProperties.Endpoint"),
+				Transform:   transform.FromField("Properties.Endpoint"),
 			},
 			{
 				Name:        "identity_type",
@@ -118,7 +119,7 @@ func tableAzureEventGridDomain(_ context.Context) *plugin.Table {
 				Name:        "input_schema",
 				Description: "This determines the format that Event Grid should expect for incoming events published to the Event Grid Domain Resource. Possible values include: 'EventGridSchema', 'CustomEventSchema', 'CloudEventSchemaV10'.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("DomainProperties.InputSchema"),
+				Transform:   transform.FromField("Properties.InputSchema"),
 			},
 			{
 				Name:        "last_modified_at",
@@ -153,7 +154,7 @@ func tableAzureEventGridDomain(_ context.Context) *plugin.Table {
 				Name:        "public_network_access",
 				Description: "This determines if traffic is allowed over public network. By default it is enabled.",
 				Type:        proto.ColumnType_STRING,
-				Transform:   transform.FromField("DomainProperties.PublicNetworkAccess"),
+				Transform:   transform.FromField("Properties.PublicNetworkAccess"),
 			},
 			{
 				Name:        "sku_name",
@@ -178,19 +179,25 @@ func tableAzureEventGridDomain(_ context.Context) *plugin.Table {
 				Name:        "inbound_ip_rules",
 				Description: "This can be used to restrict traffic from specific IPs instead of all IPs. Note: These are considered only if PublicNetworkAccess is enabled.",
 				Type:        proto.ColumnType_JSON,
-				Transform:   transform.FromField("DomainProperties.InboundIPRules"),
+				Transform:   transform.FromField("Properties.InboundIPRules"),
 			},
 			{
 				Name:        "input_schema_mapping",
 				Description: "Information about the InputSchemaMapping which specified the info about mapping event payload.",
 				Type:        proto.ColumnType_JSON,
-				Transform:   transform.FromField("DomainProperties.InputSchemaMapping"),
+				Transform:   transform.FromField("Properties.InputSchemaMapping"),
 			},
 			{
 				Name:        "private_endpoint_connections",
 				Description: "List of private endpoint connections.",
 				Type:        proto.ColumnType_JSON,
 				Transform:   transform.From(extractEventgridDomainPrivaterEndPointConnections),
+			},
+			{
+				Name:        "minimum_tls_version_allowed",
+				Description: "This determines the minimum TLS version required for traffic to the domain.",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Properties.MinimumTLSVersionAllowed").Transform(transformToString),
 			},
 
 			// Steampipe standard columns
@@ -232,42 +239,40 @@ func tableAzureEventGridDomain(_ context.Context) *plugin.Table {
 //// LIST FUNCTION
 
 func listEventGridDomains(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	plugin.Logger(ctx).Trace("listEventGridDomains")
+	logger := plugin.Logger(ctx)
+	logger.Trace("listEventGridDomains")
 
-	// Create session
-	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	session, err := GetNewSessionUpdated(ctx, d)
+	if err != nil {
+		logger.Error("azure_eventgrid_domain.listEventGridDomains", "session_error", err)
+		return nil, err
+	}
+
+	f, err := armeventgrid.NewClientFactory(session.SubscriptionID, session.Cred, session.ClientOptions)
 	if err != nil {
 		return nil, err
 	}
 
-	subscriptionID := session.SubscriptionID
-	client := eventgrid.NewDomainsClientWithBaseURI(session.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = session.Authorizer
+	client := f.NewDomainsClient()
 
-	// Apply Retry rule
-	ApplyRetryRules(ctx, &client, d.Connection)
-
-	result, err := client.ListBySubscription(ctx, "", nil)
-	if err != nil {
-		plugin.Logger(ctx).Error("listEventGridDomains", "ListBySubscription", err)
-		return nil, err
-	}
-
-	for _, domain := range result.Values() {
-		d.StreamListItem(ctx, domain)
-	}
-
-	for result.NotDone() {
-		// Wait for rate limiting
+	pager := client.NewListBySubscriptionPager(nil)
+	for pager.More() {
+		// Wait for rate limiter
 		d.WaitForListRateLimit(ctx)
 
-		err = result.NextWithContext(ctx)
+		resp, err := pager.NextPage(ctx)
 		if err != nil {
+			logger.Error("error listing next page", "api_error", err)
 			return nil, err
 		}
 
-		for _, domain := range result.Values() {
-			d.StreamListItem(ctx, domain)
+		for _, v := range resp.Value {
+			d.StreamListItem(ctx, v)
+
+			// Check if the context has been canceled or if the limit has been hit (if specified)
+			if d.RowsRemaining(ctx) == 0 {
+				return nil, nil
+			}
 		}
 	}
 
@@ -277,7 +282,8 @@ func listEventGridDomains(ctx context.Context, d *plugin.QueryData, _ *plugin.Hy
 //// HYDRATE FUNCTIONS
 
 func getEventGridDomain(ctx context.Context, d *plugin.QueryData, _ *plugin.HydrateData) (interface{}, error) {
-	plugin.Logger(ctx).Trace("getEventGridDomain")
+	logger := plugin.Logger(ctx)
+	logger.Trace("getEventGridDomain")
 
 	name := d.EqualsQuals["name"].GetStringValue()
 	resourceGroup := d.EqualsQuals["resource_group"].GetStringValue()
@@ -287,25 +293,25 @@ func getEventGridDomain(ctx context.Context, d *plugin.QueryData, _ *plugin.Hydr
 		return nil, nil
 	}
 
-	// Create session
-	session, err := GetNewSession(ctx, d, "MANAGEMENT")
+	session, err := GetNewSessionUpdated(ctx, d)
 	if err != nil {
-		return nil, err
-	}
-	subscriptionID := session.SubscriptionID
-	client := eventgrid.NewDomainsClientWithBaseURI(session.ResourceManagerEndpoint, subscriptionID)
-	client.Authorizer = session.Authorizer
-
-	// Apply Retry rule
-	ApplyRetryRules(ctx, &client, d.Connection)
-
-	op, err := client.Get(ctx, resourceGroup, name)
-	if err != nil {
-		plugin.Logger(ctx).Error("getEventGridDomain", "get", err)
+		logger.Error("azure_eventgrid_topic.listEventGridTopics", "session_error", err)
 		return nil, err
 	}
 
-	return op, nil
+	f, err := armeventgrid.NewClientFactory(session.SubscriptionID, session.Cred, session.ClientOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	client := f.NewDomainsClient()
+	op, err := client.Get(ctx, resourceGroup, name, nil)
+	if err != nil {
+		logger.Error("getEventGridDomain", "get", err)
+		return nil, err
+	}
+
+	return op.Domain, nil
 }
 
 func listEventGridDomainDiagnosticSettings(ctx context.Context, d *plugin.QueryData, h *plugin.HydrateData) (interface{}, error) {
@@ -358,10 +364,10 @@ func listEventGridDomainDiagnosticSettings(ctx context.Context, d *plugin.QueryD
 // If we return the private endpoint connection directly from api response we will not receive all the properties of private endpoint connections.
 func extractEventgridDomainPrivaterEndPointConnections(ctx context.Context, d *transform.TransformData) (interface{}, error) {
 	plugin.Logger(ctx).Trace("extractEventgridDomainPrivaterEndPointConnections")
-	domain := d.HydrateItem.(eventgrid.Domain)
+	domain := d.HydrateItem.(*armeventgrid.Domain)
 	var privateEndpointConnectionsInfo []map[string]interface{}
-	if domain.PrivateEndpointConnections != nil {
-		privateEndpointConnections := *domain.PrivateEndpointConnections
+	if domain.Properties.PrivateEndpointConnections != nil {
+		privateEndpointConnections := domain.Properties.PrivateEndpointConnections
 		for _, endpoint := range privateEndpointConnections {
 			objectMap := make(map[string]interface{})
 			if endpoint.ID != nil {
@@ -376,27 +382,27 @@ func extractEventgridDomainPrivaterEndPointConnections(ctx context.Context, d *t
 				objectMap["type"] = endpoint.Type
 			}
 
-			if endpoint.PrivateEndpointConnectionProperties != nil {
-				if endpoint.PrivateEndpointConnectionProperties.PrivateEndpoint != nil {
-					if endpoint.PrivateEndpointConnectionProperties.PrivateEndpoint.ID != nil {
-						objectMap["endpointId"] = endpoint.PrivateEndpointConnectionProperties.PrivateEndpoint.ID
+			if endpoint.Properties != nil {
+				if endpoint.Properties.PrivateEndpoint != nil {
+					if endpoint.Properties.PrivateEndpoint.ID != nil {
+						objectMap["endpointId"] = *endpoint.Properties.PrivateEndpoint.ID
 					}
 				}
-				if endpoint.PrivateEndpointConnectionProperties.GroupIds != nil {
-					objectMap["groupIds"] = endpoint.PrivateEndpointConnectionProperties.GroupIds
+				if endpoint.Properties.GroupIDs != nil {
+					objectMap["groupIds"] = endpoint.Properties.GroupIDs
 				}
-				if endpoint.PrivateEndpointConnectionProperties.ProvisioningState != "" {
-					objectMap["provisioningState"] = endpoint.PrivateEndpointConnectionProperties.ProvisioningState
+				if endpoint.Properties.ProvisioningState != nil {
+					objectMap["provisioningState"] = *endpoint.Properties.ProvisioningState
 				}
-				if endpoint.PrivateEndpointConnectionProperties.PrivateLinkServiceConnectionState != nil {
-					if endpoint.PrivateEndpointConnectionProperties.PrivateLinkServiceConnectionState.Status != "" {
-						objectMap["privateLinkServiceConnectionStateStatus"] = endpoint.PrivateEndpointConnectionProperties.PrivateLinkServiceConnectionState.Status
+				if endpoint.Properties.PrivateLinkServiceConnectionState != nil {
+					if endpoint.Properties.PrivateLinkServiceConnectionState.Status != nil {
+						objectMap["privateLinkServiceConnectionStateStatus"] = *endpoint.Properties.PrivateLinkServiceConnectionState.Status
 					}
-					if endpoint.PrivateEndpointConnectionProperties.PrivateLinkServiceConnectionState.Description != nil {
-						objectMap["privateLinkServiceConnectionStateDescription"] = endpoint.PrivateEndpointConnectionProperties.PrivateLinkServiceConnectionState.Description
+					if endpoint.Properties.PrivateLinkServiceConnectionState.Description != nil {
+						objectMap["privateLinkServiceConnectionStateDescription"] = endpoint.Properties.PrivateLinkServiceConnectionState.Description
 					}
-					if endpoint.PrivateEndpointConnectionProperties.PrivateLinkServiceConnectionState.ActionsRequired != nil {
-						objectMap["privateLinkServiceConnectionStateActionsRequired"] = endpoint.PrivateEndpointConnectionProperties.PrivateLinkServiceConnectionState.ActionsRequired
+					if endpoint.Properties.PrivateLinkServiceConnectionState.ActionsRequired != nil {
+						objectMap["privateLinkServiceConnectionStateActionsRequired"] = endpoint.Properties.PrivateLinkServiceConnectionState.ActionsRequired
 					}
 				}
 			}
